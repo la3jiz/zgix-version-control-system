0d69f02f1dcf8e7d7bdb6424f50928ceb9b93a1a
la3jiz <lazizamin8@gmail.com>
2025-08-18 23:54:59 +0200

    add staging, commit and log functionalities

--- zgix.mjs
+++ zgix.mjs
@@ -1,43 +1,131 @@
-import path from "path" 
-import fs from 'fs/promises';
-import crypto from "crypto"
-class Zgix{
-    constructor(repoPath="."){
-        this.repoPath =path.join(repoPath, ".zgix");//this will create the .zgix directory in the specified repoPath
-        this.objectsPath = path.join(this.repoPath, "objects"); //this will create the objects directory in the .zgix directory *** this is for the hashed content
-        this.headPath = path.join(this.repoPath, "HEAD"); //this will create the HEAD file in the .zgix directory*** this is for keeping track of the head
-        this.indexPath = path.join(this.repoPath, "index"); //this will create the index file in the .zgix directory*** this is for the staging area
-        this.init();
-    }
+import path from "path";
+import fs from "fs/promises";
+import crypto from "crypto";
+class Zgix {
+  constructor(repoPath = ".") {
+    this.repoPath = path.join(repoPath, ".zgix"); //this will create the .zgix directory in the specified repoPath
+    this.objectsPath = path.join(this.repoPath, "objects"); //this will create the objects directory in the .zgix directory *** this is for the hashed content
+    this.headPath = path.join(this.repoPath, "HEAD"); //this will create the HEAD file in the .zgix directory*** this is for keeping track of the head
+    this.indexPath = path.join(this.repoPath, "index"); //this will create the index file in the .zgix directory*** this is for the staging area
+    this.init();
+  }
 
-    async init(){
-        await fs.mkdir(this.objectsPath, { recursive: true });
-        try {
-            await fs.writeFile(this.headPath, "",{flag: 'wx'}); //wx: open for writing , fail if file exists
-            await fs.writeFile(this.indexPath, JSON.stringify([]),{flag: 'wx'}); //wx: open for writing , fail if file exists
-        } catch (error) {
-            console.error("Error initializing zgix:", error);
-        }   
+  async init() {
+    try {
+      await fs.mkdir(this.objectsPath, { recursive: true });
+      await fs.writeFile(this.headPath, "", { flag: "wx" }); //wx: open for writing , fail if file exists
+      await fs.writeFile(this.indexPath, JSON.stringify([]), { flag: "wx" }); //wx: open for writing , fail if file exists
+    } catch (error) {
+      // console.error("zgix already exist");
     }
+  }
 
-    hasObject(content){
-        return crypto.createHash('sha1').update(content, 'utf-8').digest('hex');
+  // Hash the content of a file
+  hasObject(content) {
+    return crypto.createHash("sha1").update(content, "utf-8").digest("hex");
+  }
+
+  async add(fileToBeAdded) {
+    try {
+      const fileData = await fs.readFile(fileToBeAdded, "utf-8");
+      const fileHash = this.hasObject(fileData);
+      const objectsDirPath = path.join(this.objectsPath, fileHash.slice(0, 2));
+      await fs.mkdir(objectsDirPath, { recursive: true });
+      const objectsFilePath = path.join(objectsDirPath, fileHash.slice(2));
+      await fs.writeFile(objectsFilePath, fileData);
+      await this.updatingStagingArea(fileToBeAdded, fileHash);
+      // console.log(`Added file to zgix: ${fileToBeAdded}`);
+    } catch (err) {
+      console.error("Error adding file to zgix:", err);
     }
+  }
 
-    async add(fileToBeAdded){
-        const fileData=await fs.readFile(fileToBeAdded, "utf-8");
-        console.log("File Data:", fileData);
-        const fileHash=this.hasObject(fileData);
-        console.log("File Hash:", fileHash);
-        const objectsDirPath=path.join(this.objectsPath, fileHash.slice(0, 2));
-        const objectsFilePath=path.join(objectsDirPath, fileHash.slice(2));
-        await fs.mkdir(objectsDirPath, { recursive: true });
-        await fs.writeFile(objectsFilePath, fileData);
-        //TODO: missing step : add the file to staging area
-        console.log(`Added file to zgix: ${fileToBeAdded}`);
+  //adding the staging area inside index file
+  async updatingStagingArea(filePath, fileHash) {
+    try {
+      const index = JSON.parse(
+        await fs.readFile(this.indexPath, { encoding: "utf-8" })
+      );
+      index.push({ path: filePath, hash: fileHash });
+      await fs.writeFile(this.indexPath, JSON.stringify(index));
+    } catch (err) {
+      console.error("Error updating staging area:", err);
     }
+  }
 
+  async commit(message) {
+    try {
+      const index = JSON.parse(
+        await fs.readFile(this.indexPath, { encoding: "utf-8" })
+      );
+      const parentCommit = await this.getCurrentHead();
+      const commitData = {
+        message,
+        author: "User",
+        timeStamp: new Date().toISOString(),
+        files: index,
+        parent: parentCommit,
+      };
+      const commitHash = this.hasObject(JSON.stringify(commitData));
+      const commitDirPath = path.join(this.objectsPath, commitHash.slice(0, 2));
+      await fs.mkdir(commitDirPath, { recursive: true });
+      const commitFilePath = path.join(commitDirPath, commitHash.slice(2));
+      await fs.writeFile(commitFilePath, JSON.stringify(commitData));
+      await fs.writeFile(this.headPath, commitHash); // update the head to point to the new commit
+      await fs.writeFile(this.indexPath, JSON.stringify([])); //clear the staging area
+      // console.log(`Commit created: ${commitHash}`);
+    } catch (err) {
+      console.error("Error committing changes:", err);
+    }
+  }
+
+  async getCurrentHead() {
+    try {
+      const head = await fs.readFile(this.headPath, { encoding: "utf-8" });
+      return head.trim();
+    } catch (err) {
+      return null; // If HEAD file empty, return null
+    }
+  }
+
+  async log() {
+    let currentCommitHash = await this.getCurrentHead();
+    let isHead = true;
+    while (currentCommitHash) {
+      const commitDirPath = path.join(
+        this.objectsPath,
+        currentCommitHash.slice(0, 2)
+      );
+      const commitFilePath = path.join(
+        commitDirPath,
+        currentCommitHash.slice(2)
+      );
+      const commitData = JSON.parse(
+        await fs.readFile(commitFilePath, { encoding: "utf-8" })
+      );
+      console.log(
+        `\x1b[33mcommit ${currentCommitHash}\x1b[0m` +
+          (isHead ? `\x1b[32m (HEAD)\x1b[0m` : "")
+      );
+      console.log(`\x1b[34mAuthor:\x1b[0m ${commitData.author}`);
+      console.log(`\x1b[35mDate:\x1b[0m   ${commitData.timeStamp}\n`);
+      console.log(`    ${commitData.message}\n`);
+
+      if (commitData.parent) {
+        console.log(
+          "\x1b[90m------------------------------------------------------------\x1b[0m\n"
+        );
+      }
+
+      isHead = false;
+      currentCommitHash = commitData.parent;
+    }
+  }
 }
 
-const zgix=new Zgix();
-zgix.add("sample.txt");
+(async () => {
+  const zgix = new Zgix();
+  await zgix.add("sample.txt");
+  await zgix.commit("fourth commit");
+  await zgix.log();
+})();
